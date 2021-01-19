bf8ee72b245e6c9d97f63cdcc8c6816beae19bfe
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-01-19 10:49:50 +0800

    'l'

--- src/views/page1.vue
+++ src/views/page1.vue
@@ -2,7 +2,8 @@
   <div>
     {{ name }} {{ counter }}
     <p @click="changeName">改变</p>
-    {{age}}
+    <p @click="changeAge">改变年龄：</p>
+    年龄：{{age}}
   </div>
 </template>
 
@@ -22,18 +23,21 @@ export default {
     const counter = ref(0);
     console.log("props===>", props);
     var state = reactive({
-      age:0
+      age:0,
+      changeAge() {
+        state.age = state.age +7
+      }
     })
     const { name } = toRefs(props.test);
-    function changeName() {
-      console.log(33131);
-      counter.value++;
-      name.value = 'dada'
-      state.age++
-    }
+    // function changeName() {
+    //   console.log(33131);
+    //   counter.value++;
+    //   name.value = 'dada'
+    //   // state.age++
+    // }
     return {
       ...toRefs(state),
-      changeName,
+      // changeName,
       counter,
       name,
     };
--- src/views/page2.vue
+++ src/views/page2.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>page2的值是:{{ number }}</div>
+  <div>
+    page2的值是:{{ name }}
+    <p @click="changeName">dfasdasd</p>
+    {{changeData1}}
+  </div>
 </template>
 
 <script>
+import { toRefs, ref, onRenderTracked, toRaw, reactive } from "vue";
 export default {
   data() {
     return {};
+  },
+  setup() {
+    const data1 = reactive({
+      name: "tmc",
+      age: 25,
+    });
+    const data2 = ref(666);
+
+    const changeData1 = toRaw(data1.name);
+    const changeData2 = toRaw(data2.value);
+
+    onRenderTracked((e) => {
+      console.log(e);
+      console.log("状态跟踪");
+    });
+    function changeName() {
+      data1.name = "99999";
+      console.log("dadada", changeData1);
+    }
+    return {
+      ...toRefs(data1),
+      ...toRefs(data2),
+      changeName,
+      changeData1
+    };
   },
 };
 </script>
